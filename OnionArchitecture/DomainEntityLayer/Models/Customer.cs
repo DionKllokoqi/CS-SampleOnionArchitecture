f77904c86bb6189201d57fdc11897c15b0f11e30
dion <dion.kllokoqi@gmail.com>
2022-01-09 16:19:50 +0100

    Adds Customer entity mapper.

--- OnionArchitecture/DomainEntityLayer/Models/Customer.cs
+++ OnionArchitecture/DomainEntityLayer/Models/Customer.cs
@@ -3,7 +3,7 @@
     public class Customer : BaseEntity
     {
         public string CustomerName { get; set; }
-        public string PurchasesProduct { get; set; }
+        public string PurchasedProduct { get; set; }
         public string PaymentType { get; set; }
     }
 }
