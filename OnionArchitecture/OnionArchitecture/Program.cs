4694e8b8aa21ba3f913d35109bdbb808326da769
dion <dion.kllokoqi@gmail.com>
2022-01-10 14:20:06 +0100

    Adds repository layer and inital context migration.

--- OnionArchitecture/OnionArchitecture/Program.cs
+++ OnionArchitecture/OnionArchitecture/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "OnionArchitecture", Version = "v1" });
+});
+
+#region Connection String
+builder.Services.AddDbContext<ApplicationDbContext>(item => item.UseSqlServer("name=ConnectionStrings:localOnionSql"));
+#endregion
 
 var app = builder.Build();
 
---
+++ OnionArchitecture/RepositoryLayer/RepositoryPattern/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainEntityLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RepositoryLayer.RepositoryPattern
+{
+    public class Repository<T> : IRepository<T> where T: BaseEntity
+    {
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetT(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
